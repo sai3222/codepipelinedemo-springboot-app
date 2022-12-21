cfd5ade30de2651fba0fc5a43f7f7e5797758699
Sai Krishna <84509777+sai3222@users.noreply.github.com>
2022-12-21 14:59:27 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-#_Change_Working_Directory
-cd /home/ubuntu
-
-#_Update_&_Set_Node_Version
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-
-#_Download_Node_&NPM
-apt -y install nodejs npm
-# install apach2
-apt install apache2 -y
-
-#_Download_PM2
-npm install pm2@latest -g
+apt install nginx -y
+systemctl status nginx
