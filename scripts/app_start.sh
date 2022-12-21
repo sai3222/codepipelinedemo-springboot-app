27b093cbaa8f968621acf7378603a3d3d322bd23
Sai Krishna <84509777+sai3222@users.noreply.github.com>
2022-12-21 15:02:06 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-#_Change_Working_Directory
-cd /home/ubuntu
-
-#_Delete_Old_PM2_Service
-#sudo pm2 delete Frontend
-#sudo pm2 start server.js --name Frontend
-pm2 delete Frontend
-pm2 start server.js --name Frontend
+echo "our applicataion is deployed"
