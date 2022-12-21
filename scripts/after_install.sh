751bc0cbb2aa407cf9edfb9fb8787582e4cb3c5f
Sai Krishna <84509777+sai3222@users.noreply.github.com>
2022-12-21 15:00:06 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-#_Change_Working_Directory
-cd /home/ubuntu
-
-#_Remove_Unused_Code
-rm -rf node_modules
-rm -rf build
-
-#Install_node_modules_&_Make_React_Build
-npm  install
-npm run build
+echo "hello world"
